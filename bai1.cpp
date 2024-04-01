c8c9ef2c3fc717380f79844160ce3331c55db563
Cealius <134500760+Cealius@users.noreply.github.com>
2024-04-01 09:53:46 +0700

    Add files via upload

---
+++ bai1.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void){
+	// Your code here!
+	int i;
+	printf("Cac so nguyen co 2 chu so va la boi cua 7:\n");
+	for (i = 14; i <= 98; i += 7)
+	{
+		printf("%d\n", i);
+	}
+	return 0;
+}
